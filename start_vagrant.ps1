c1e8e2a0b28421939e8dc204b6110ed0cd33103c
PixelFirebird <138672238+PixelFirebird@users.noreply.github.com>
2023-07-06 16:45:05 +0100

    Update start_vagrant.ps1

--- start_vagrant.ps1
+++ start_vagrant.ps1
@@ -1,5 +1,10 @@
 # Download Vagrant file
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PixelFirebird/Valkyrie/main/Vagrantfile" -OutFile ".\Vagrantfile"
 
+# Create synced folder
+if (!(Test-Path -Path .\mevn-app)) {
+    New-Item -ItemType Directory -Path .\mevn-app
+}
+
 # Start Vagrant
 vagrant up
